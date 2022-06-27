4bd5c94dd21a209aad3018623d4f4248c3ad99cd
Rodrigo Bertozzi de Castro <rbertozzicastro1@hotmail.com>
2022-06-27 20:37:21 -0300

    Realizado o primeiro exercicio

---
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Lasagna
+{
+    public int ExpectedMinutesInOven() => 40;
+    public int RemainingMinutesInOven(int remainingMinutesInOven) => 40 - remainingMinutesInOven;
+    public int PreparationTimeInMinutes(int layers) => layers * 2;
+    public int ElapsedTimeInMinutes(int layers, int minutesInOven) => layers * 2 + minutesInOven;
+
+}
