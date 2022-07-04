d735df97f3fe63c63e001492754e5fce5e0f19dd
Rodrigo Bertozzi de Castro <rbertozzicastro1@hotmail.com>
2022-07-04 16:34:21 -0300

    Feito o exercicio ElonsToys

--- csharp/elons-toys/ElonsToys.cs
+++ csharp/elons-toys/ElonsToys.cs
@@ -2,23 +2,33 @@ using System;
 
 class RemoteControlCar
 {
+    public int distanceDisplay = 0;
+    public int batteryDisplay = 100;
     public static RemoteControlCar Buy()
-    {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Buy() method");
-    }
+    => new RemoteControlCar();
 
     public string DistanceDisplay()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDisplay() method");
-    }
+        => $"Driven {distanceDisplay} meters";
+
 
     public string BatteryDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
+        if (batteryDisplay == 0)
+            return "Battery empty";
+        else
+            return $"Battery at {batteryDisplay}%";
     }
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if (batteryDisplay == 0)
+        {
+            distanceDisplay = distanceDisplay + 0;
+        }
+        else
+        {
+            distanceDisplay = distanceDisplay + 20;
+            batteryDisplay = batteryDisplay - 1;
+        }
     }
 }
