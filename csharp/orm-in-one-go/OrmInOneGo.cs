33f2aaeae1be384ced8a01ea79d41d153cbdfba4
Rodrigo Bertozzi de Castro <rbertozzicastro1@hotmail.com>
2022-07-19 15:11:21 -0300

    Feito o exercicio sobre using

---
+++ csharp/orm-in-one-go/OrmInOneGo.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+
+public class Orm
+{
+    private Database database;
+
+    public Orm(Database database)
+    {
+        this.database = database;
+    }
+
+    public void Write(string data)
+    {
+        using (database)
+        {
+            database.BeginTransaction();
+            database.Write(data);
+            database.EndTransaction();
+        }
+    }
+
+    public bool WriteSafely(string data)
+    {
+        using (database)
+        {
+            try
+            {
+                database.BeginTransaction();
+                database.Write(data);
+                database.EndTransaction();
+                return true;
+            }
+            catch (InvalidOperationException)
+            {
+                database.Dispose();
+                return false;
+            }
+        }
+    }
+}
