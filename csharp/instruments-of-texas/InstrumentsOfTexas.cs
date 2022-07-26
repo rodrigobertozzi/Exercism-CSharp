8ba62745b394dd3b293fac4c5b659ba897b1ef47
Rodrigo Bertozzi de Castro <rbertozzicastro1@hotmail.com>
2022-07-26 11:11:02 -0300

    Feito o exercicio sobre User Defined Exception

---
+++ csharp/instruments-of-texas/InstrumentsOfTexas.cs
@@ -0,0 +1,68 @@
+using System;
+
+public class CalculationException : Exception
+{
+    public CalculationException(int operand1, int operand2, string message, Exception inner) :
+        base(message, inner)
+    {
+        Operand1 = operand1;
+        Operand2 = operand2;
+    }
+
+    public int Operand1 { get; }
+    public int Operand2 { get; }
+}
+
+public class CalculatorTestHarness
+{
+    private Calculator calculator;
+
+    public CalculatorTestHarness(Calculator calculator)
+    {
+        this.calculator = calculator;
+    }
+
+    public string TestMultiplication(int x, int y)
+    {
+        try
+        {
+            this.Multiply(x, y);
+            return "Multiply succeeded";
+        }
+        catch (CalculationException ex) when (ex.Operand1 < 0 && ex.Operand2 < 0)
+        {
+            return $"Multiply failed for negative operands. {ex.InnerException.Message}";
+        }
+        catch (CalculationException ex)
+        {
+            return $"Multiply failed for mixed or positive operands. {ex.InnerException.Message}";
+        }
+    }
+
+    public void Multiply(int x, int y)
+    {
+        try
+        {
+            this.calculator.Multiply(x, y);
+        }
+        catch (OverflowException e)
+        {
+            throw new CalculationException(x, y, "error in Multiply", e);
+        }
+    }
+}
+
+
+// Please do not modify the code below.
+// If there is an overflow in the multiplication operation
+// then a System.OverflowException is thrown.
+public class Calculator
+{
+    public int Multiply(int x, int y)
+    {
+        checked
+        {
+            return x * y;
+        }
+    }
+}
