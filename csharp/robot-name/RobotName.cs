4505a511a5d74ff73296458791c072cc515763da
Rodrigo Bertozzi de Castro <rbertozzicastro1@hotmail.com>
2022-08-01 10:49:00 -0300

    Feito o exertcicio sobre genar strings unicas

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,26 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 public class Robot
 {
-    public Robot() => Name = GenerateName();
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }
-    }
+    public Robot() => Reset();
+    public string Name{ get; private set; }
 
-    public string name;
+    private static readonly HashSet<string> RobotNameTracker = new HashSet<string>();
 
     public string GenerateName()
     {
-        string randName = @"^[A-Z]{2}\d{3}$";
-        Random rand = new Random();
-        
-        // I don't know how to implement this
+        Random random = new Random();
+        var generateName = new char[5];
+
+        for(var i = 0; i < 2; i++)
+        {
+            generateName[i] = (char)random.Next('A', 'Z' + 1);
+        }
+
+        for(var i = 2; i < 5; i++)
+        {
+            generateName[i] = (char)random.Next('0', '9' + 1);
+        }
+        return new string(generateName);
     }
 
-    public void Reset()
+    public string GenerateUniqueName()
     {
-        
+        var name = Name;
+
+        while (!RobotNameTracker.Add(name))
+            name = GenerateName();
+
+        return name;
     }
+
+    public void Reset() => Name = GenerateUniqueName();
 }
